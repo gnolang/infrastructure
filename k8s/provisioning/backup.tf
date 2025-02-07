138a4527e11e129703416e1d963854aa0a8f0f6f
Sergio Maria Matone <sw360cab@gmail.com>
2025-02-07 18:16:21 +0100

    fixing terraform backup rules

--- k8s/provisioning/backup.tf
+++ k8s/provisioning/backup.tf
@@ -1,33 +1,32 @@
-
-data "aws_backup_vault" "gno_vault" {
-  name = "testnet-data-vault"
-}
-
 data "aws_iam_role" "eks_gno_backup" {
   name = "eksBackupEbsRole"
 }
 
-resource "aws_backup_plan" "testnet-backup" {
-  name = "testnets_backup_plan"
+resource "aws_backup_vault" "gno_vault" {
+  name = "${var.gno_project}-backup-vault"
+}
+
+resource "aws_backup_plan" "testnet_backup" {
+  name = "${var.gno_project}-backup-plan"
 
   rule {
-    rule_name         = "testnet-backup-rule"
-    target_vault_name = data.aws_backup_vault.gno_vault.name
+    rule_name         = "${var.gno_project}-backup-rule"
+    target_vault_name = aws_backup_vault.gno_vault.name
     schedule          = "cron(0 4 * * ? *)"
 
-    start_window = 60
+    start_window      = 60
     completion_window = 120
     lifecycle {
       cold_storage_after = 10
-      delete_after = 100
+      delete_after       = 100
     }
   }
 }
 
 resource "aws_backup_selection" "ebs_tagged_resources" {
-  name         = "testnets_backup_selection"
+  name         = "${var.gno_project}-backup-selection"
   iam_role_arn = data.aws_iam_role.eks_gno_backup.arn
-  plan_id      = aws_backup_plan.testnet-backup.id
+  plan_id      = aws_backup_plan.testnet_backup.id
   resources    = ["arn:aws:ec2:*:*:volume/*"]
 
   condition {
@@ -36,10 +35,10 @@ resource "aws_backup_selection" "ebs_tagged_resources" {
       value = "validator-storage"
     }
     string_equals {
-      key   = "aws:ResourceTag/Name"
-      value = "validator-storage"
+      key   = "aws:ResourceTag/Project"
+      value = var.gno_project
     }
   }
 
-  depends_on = [ aws_backup_plan.testnet-backup ]
+  depends_on = [aws_backup_plan.testnet_backup]
 }
